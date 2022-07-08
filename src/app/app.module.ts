9cf97e005513e9999d25b9cc5382aa0d045801ca
Sree1710 <kunjusree817@gmail.com>
2022-07-08 14:12:43 +0530

    view-todo table completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewTodoComponent } from './view-todo/view-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTodoComponent
   ],
   imports: [
     BrowserModule,
